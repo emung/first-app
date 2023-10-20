141b2a7cf4b2503c02dc4d7f1f595341a5090b33
emung <emanuel.ungheanu@hotmail.com>
2023-10-20 11:44:46 +0200

    change imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Injectable, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
