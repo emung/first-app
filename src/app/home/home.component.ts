5ae65e533d096d66fe515dad40186b33211d8abe
emung <emanuel.ungheanu@hotmail.com>
2023-10-20 13:23:43 +0200

    lesson 13 search feature for list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,9 +19,22 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
 
   constructor(private housingService: HousingService) {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string, event: Event) {
+    event.preventDefault();
+
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
   }
 
 }
